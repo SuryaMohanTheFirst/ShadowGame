d21da695095a9cbacb5e2705ac900f98e085d8c8
DanielCefaratti <cefaratti.daniel94@gmail.com>
2020-01-24 15:50:39 -0500

    Timer + GitIgnore
    Inital timer, change swapTime value to change time between light and dark. Also gitignore commit. Might need changes in future.

--- shadow_project/Shadow project/Assets/scripts/LightManager.cs
+++ shadow_project/Shadow project/Assets/scripts/LightManager.cs
@@ -6,6 +6,8 @@ public class LightManager : MonoBehaviour
 {
     private int randVal;
     private int remainder;
+    public float swapTime;
+    public GameObject backingLight;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +18,31 @@ public class LightManager : MonoBehaviour
         Debug.Log(remainder);
         if (remainder == 1)
         {
-            
+            backingLight.SetActive(true);
         }
         else if (remainder == 0)
         {
-
+            backingLight.SetActive(false);
         }
+
+        InvokeRepeating("swapLight", swapTime, swapTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void swapLight()
+    {
+        print(backingLight.activeSelf);
+        if (backingLight.activeSelf)
+        {
+            backingLight.SetActive(false);
+        } else
+        {
+            backingLight.SetActive(true);
+        }
     }
 }
