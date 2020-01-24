decc904b511f7d249d5a6fa7d5a3c2efabd8bc32
ChrisColalillo <christiancolalillo@gmail.com>
2020-01-24 14:34:48 -0500

    Daniel doin stuff- changed lighting and some names

---
+++ shadow_project/Shadow project/Assets/scripts/LightManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    private int randVal;
+    private int remainder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randVal = Random.Range(1, 11);
+        Debug.Log(randVal);
+        remainder = randVal % 2;
+        Debug.Log(remainder);
+        if (remainder == 1)
+        {
+            
+        }
+        else if (remainder == 0)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
